9a40c0b50341beb13779709d1f8395716c61bd27
Carl Michael Marte <49306017+martecarl@users.noreply.github.com>
2024-12-13 11:16:30 +0800

    debugging

--- mainapp.py
+++ mainapp.py
@@ -15,7 +15,8 @@ class_names = ['ArtDeco', 'Ethnic', 'Special', 'Traditional']
 
 def capture_image_with_preview():
     # Open the camera using OpenCV
-    cap = cv2.VideoCapture(0)  # 0 is the default camera index
+    cap = cv2.VideoCapturecap('libcamerasrc ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+  # 0 is the default camera index
     if not cap.isOpened():
         print("Error: Unable to access the camera.")
         return None
